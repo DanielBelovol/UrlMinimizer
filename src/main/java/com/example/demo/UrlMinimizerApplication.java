ab3b08ad52befd366437c200fa7ea2034d8c5eb0
DanielBelovol <danielbelovol08@gmail.com>
2024-04-12 19:43:28 +0300

    start making project

---
+++ src/main/java/com/example/demo/UrlMinimizerApplication.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlMinimizerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UrlMinimizerApplication.class, args);
+	}
+}
