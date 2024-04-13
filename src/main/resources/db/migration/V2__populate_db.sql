0c520381e6e85fad5cd170d7155be13cbcfd1d02
Vladimir <karakisvladimir@gmail.com>
2024-04-13 23:11:43 +0300

    upgrade DB

--- src/main/resources/db/migration/V2__populate_db.sql
+++ src/main/resources/db/migration/V2__populate_db.sql
@@ -1,8 +1,13 @@
 INSERT INTO users (username, password, email, created_date)
-VALUES ('admin', '$2a$10$.IeRI/Gy/8UscmtMmMHyDe2PDe0TMLn.9vb6WUSS2FVtzGmEZzcj2', 'admin@gmail.com', NOW(), NOW()),
-       ('auth user', '$2a$10$uZ/R0BFKt2MqlybEAcO2NebJkdp5qWI3G2SnssNn3HcMHEZAC61Nu', 'auth_user@gmail.com', NOW(), NOW()),
-       ('user', '$2a$10$AcO2NebJkdp5qWI3dp50TMLn.9vbHcMHEZRI/Gy/8Uscmtgfds54', 'user@gmail.com', NOW(), NOW());
+VALUES ('admin', '$2a$10$.IeRI/Gy/8UscmtMmMHyDe2PDe0TMLn.9vb6WUSS2FVtzGmEZzcj2', 'admin@gmail.com',  NOW()),
+       ('auth user', '$2a$10$uZ/R0BFKt2MqlybEAcO2NebJkdp5qWI3G2SnssNn3HcMHEZAC61Nu', 'auth_user@gmail.com',  NOW()),
+       ('user', '$2a$10$AcO2NebJkdp5qWI3dp50TMLn.9vbHcMHEZRI/Gy/8Uscmtgfds54', 'user@gmail.com',  NOW());
 
-INSERT INTO roles (name) VALUES ('ADMIN'),('AUTH_USER'), ('USER');
+INSERT INTO roles (name) VALUES ('ADMIN'), ('USER');
 
-INSERT INTO user_roles (user_id, role_id) VALUES (1, 1), (2, 2), (3, 3);
+
+INSERT INTO user_roles (user_id, role_id) VALUES
+   ((SELECT id FROM users WHERE username = 'admin'), (SELECT id FROM roles WHERE name = 'ADMIN')),
+   ((SELECT id FROM users WHERE username = 'user'), (SELECT id FROM roles WHERE name = 'USER'));
+
+
