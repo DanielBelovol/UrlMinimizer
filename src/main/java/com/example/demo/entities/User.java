9c1bbdf452b7d452445c06ab4fcef9bd9a836e7c
DanielBelovol <danielbelovol08@gmail.com>
2024-04-13 13:00:16 +0300

    added userClass and packages

---
+++ src/main/java/com/example/demo/entities/User.java
@@ -0,0 +1,18 @@
+package com.example.demo.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private Date createdDate;
+}
