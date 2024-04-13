c39486999016a253048ce538087c26a75e8752bd
Vladimir <karakisvladimir@gmail.com>
2024-04-13 12:33:51 +0300

    change migration

--- src/main/resources/db/migration/V2__populate_db.sql
+++ src/main/resources/db/migration/V2__populate_db.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (username, password, email, created_date, last_updated_date)
+INSERT INTO users (username, password, email, created_date)
 VALUES ('admin', '$2a$10$.IeRI/Gy/8UscmtMmMHyDe2PDe0TMLn.9vb6WUSS2FVtzGmEZzcj2', 'admin@gmail.com', NOW(), NOW()),
        ('auth user', '$2a$10$uZ/R0BFKt2MqlybEAcO2NebJkdp5qWI3G2SnssNn3HcMHEZAC61Nu', 'auth_user@gmail.com', NOW(), NOW()),
        ('user', '$2a$10$AcO2NebJkdp5qWI3dp50TMLn.9vbHcMHEZRI/Gy/8Uscmtgfds54', 'user@gmail.com', NOW(), NOW());
