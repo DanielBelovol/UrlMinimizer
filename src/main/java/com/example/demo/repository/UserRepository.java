2910c08bcce97f30d95bfd79f4edd27195fb2d65
Vladimir <karakisvladimir@gmail.com>
2024-04-14 22:52:25 +0300

    controller

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,7 +1,9 @@
-//package com.example.demo.repository;
-//
-//import com.example.demo.entities.UserClass;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface UserRepository extends JpaRepository<Long, UserClass>{
-//}
+package com.example.demo.repository;
+
+import com.example.demo.entities.UserClass;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserClass, Long> {
+}
