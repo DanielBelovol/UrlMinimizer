ff5b2b694c43abf2bc2d263be47cf2b13da0ea02
Vladimir <karakisvladimir@gmail.com>
2024-04-15 23:51:40 +0300

    url controller

--- src/main/java/com/example/demo/repository/UrlRepository.java
+++ src/main/java/com/example/demo/repository/UrlRepository.java
@@ -1,14 +1,7 @@
 package com.example.demo.repository;
 
-import com.example.demo.entities.Url;
+import com.example.demo.entities.UrlClass;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-@Repository
-public interface UrlRepository extends JpaRepository<Url, Long> {
-
-    List<Url> findAllByUserId(Long user_id);
-    Url findByShortUrl(String shortUrl);
+public interface UrlRepository extends JpaRepository< UrlClass,Long> {
 }
