fae7539e86da0aca7aa99619574d8a8b013d03ab
Vladimir <karakisvladimir@gmail.com>
2024-04-16 22:54:08 +0300

    mini upgrade

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
+
 import com.example.demo.entities.UserClass;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,10 +13,11 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
-
+    public static final String USER_NOT_FOUND_WITH_ID = "User not found with id: ";
     @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @GetMapping
     public ResponseEntity<List<UserClass>> getAllUsers() {
@@ -25,7 +28,6 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<UserClass> getUserById(@PathVariable Long id) {
         UserClass user = userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id, "id", String.valueOf(id)));
                 .orElseThrow(() -> new ResourceNotFoundException(USER_NOT_FOUND_WITH_ID + id, "id", String.valueOf(id)));
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
