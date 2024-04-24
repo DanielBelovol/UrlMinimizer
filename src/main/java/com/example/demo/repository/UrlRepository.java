484c6192fd7020e2beffeacfd7c1d4df515c6d0c
vadosiuoso <trezstyle@gmail.com>
2024-04-25 01:23:21 +0200

    Added 2 Service layers Refactor UrlController Fixed db migration Added dtos

--- src/main/java/com/example/demo/repository/UrlRepository.java
+++ src/main/java/com/example/demo/repository/UrlRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+<<<<<<< HEAD
 import java.util.*;
 import java.util.Optional;
 import com.example.demo.entities.UrlClass;
@@ -20,4 +21,17 @@ public interface UrlRepository extends JpaRepository<UrlClass, Long> {
 
 
 
+=======
+import com.example.demo.entities.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UrlRepository extends JpaRepository<Url, Long> {
+
+    List<Url> findAllByUserId(Long user_id);
+    Url findByShortUrl(String shortUrl);
+>>>>>>> origin/thymeleaf
 }
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,19 +1,27 @@
 package com.example.demo.repository;
 
+<<<<<<< HEAD
 import java.util.List;
 import java.util.Optional;
 
 import com.example.demo.entities.UrlClass;
 import com.example.demo.entities.UserClass;
+=======
+import com.example.demo.entities.User;
+>>>>>>> origin/thymeleaf
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
+<<<<<<< HEAD
 public interface UserRepository extends JpaRepository<UserClass, Long> {
 
   @Query("SELECT u FROM UserClass u WHERE u.username = :usernameOrEmail OR u.email = :usernameOrEmail")
   Optional<UserClass> findByUsernameOrEmail(String usernameOrEmail);
 
   UserClass findByUsername(String username);
+=======
+public interface UserRepository extends JpaRepository<User, Long> {
+>>>>>>> origin/thymeleaf
 }
